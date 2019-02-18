01de4a463c65f908674ef6435e1ef19d416d2030
Jason Weirather <jason-weirather@users.noreply.github.com>
2019-02-18 14:15:10 -0500

    reflect refactor and restructure in full version tick

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_folder,'README.md'),encoding='utf-8') as inf:
 
 setup(
   name='pythologist',
-  version='0.1.10',
+  version='1.0.0',
   description='inForm PerkinElmer Reader - Python interface to read outputs of the PerkinElmer inForm software',
   long_description=long_description,
   url='https://github.com/jason-weirather/pythologist',
