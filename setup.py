648cd215bfcf7df235f0e87f87f7276420e30678
Jason Weirather <jason-weirather@users.noreply.github.com>
2019-02-18 19:23:48 -0500

    add example data

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ with open(path.join(this_folder,'README.md'),encoding='utf-8') as inf:
 setup(
   name='pythologist',
   version='1.0.0',
+  test_suite='nose2.collector.collector',
   description='inForm PerkinElmer Reader - Python interface to read outputs of the PerkinElmer inForm software',
   long_description=long_description,
   url='https://github.com/jason-weirather/pythologist',
