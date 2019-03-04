5bd15da28ffe5f9c856fb4f37c717182f40069e0
Jason L Weirather <jason-weirather@users.noreply.github.com>
2019-03-04 10:28:17 -0500

    update interface to images

--- pythologist/interface/__init__.py
+++ pythologist/interface/__init__.py
@@ -6,14 +6,14 @@ import sys, os, io
 import imageio
 from PIL import Image
 
-class ImageOutput(pd.DataFrame):
+class SegmentationImageOutput(pd.DataFrame):
     _metadata = []
     def __init__(self,*args, **kw):
-        super(ImageOutput, self).__init__(*args, **kw) 
+        super(SegmentationImageOutput, self).__init__(*args, **kw) 
         if 'verbose' in kw: self.verbose = kw['verbose']
     @property
     def _constructor(self):
-        return ImageOutput
+        return SegmentationImageOutput
     def write_to_path(self,path,suffix='',format='png',overwrite=False):
         """
         Output the data the dataframe's 'image' column to a directory structured by project->sample and named by frame
@@ -28,12 +28,12 @@ class ImageOutput(pd.DataFrame):
             else: fname = os.path.join(spath,r['frame_name']+'_'+suffix+'.'+format)
             imageio.imwrite(fname, r['image'],format=format)
 
-
-class Images(Measurement):
+class SegmentationImages(Measurement):
     def __init__(self,*args,**kwargs):
-        super(Images,self).__init__(*args,**kwargs)
+        super(SegmentationImages,self).__init__(*args,**kwargs)
         self._edge_map_cache = None
         self._cell_map_cache = None
+        self._coordinates = None
     @staticmethod
     def _preprocess_dataframe(cdf,*args,**kwargs):
         base = pd.DataFrame(cdf).copy().loc[:,cdf.frame_columns].drop_duplicates()
@@ -45,7 +45,94 @@ class Images(Measurement):
             for frame_id in base.loc[base['sample_id']==sample_id,'frame_id']:
                 f = s.get_frame(frame_id)
                 data.append([sample_id,frame_id,f.shape])
-        return base.merge(pd.DataFrame(data,columns=['sample_id','frame_id','shape']),on=['sample_id','frame_id'])
+
+
+        base = base.merge(pd.DataFrame(data,columns=['sample_id','frame_id','shape']),on=['sample_id','frame_id'])
+        return base
+
+    def write_segmentation_images(self,schema,path,suffix='',format='png',overwrite=False,background=(0,0,0,255)):
+        """
+        executes the build_segmentation_image and write_segmentation_image for each frame
+                 rather than executing on all frames at once.
+        """
+        if os.path.exists(path) and overwrite is False: raise ValueError("Error: use ovewrite=True to overwrite images")
+        for i,r in self.iterrows():
+            subcdf = self.cdf.loc[self.cdf['frame_id']==r['frame_id']].copy()
+            if self.verbose: sys.stderr.write("==============\nWriting schema for: \n"+str(r)+"\n\n")
+            subsegs = subcdf.segmentation_images(verbose=self.verbose)
+            layers = subsegs.build_segmentation_image(schema,background=background)
+            layers.write_to_path(path,overwrite=True)
+            if self.verbose: sys.stderr.write("Finished writing schema.\n")
+
+
+
+    def get_coordinates(self):
+        if self._coordinates is not None: return self._coordinates
+        df = self.set_index(self.cdf.frame_columns+['shape']).stack().\
+            reset_index().rename(columns={'level_7':'image_type',0:'image'}).\
+            set_index(self.cdf.frame_columns+['shape','image_type'])
+        imgs = []
+        for i,r in df.iterrows():
+            if self.verbose: sys.stderr.write("Extracting coordinates from "+str(list(i))+"\n")
+            left = pd.DataFrame([i],columns=df.index.names)
+            left['_key'] = 1
+            img=map_image_ids(r['image']).groupby('id').apply(lambda x: list(zip(*x[['x','y']].apply(tuple).tolist())))
+            img = img.reset_index().rename(columns={'id':'cell_index',0:'coords'})
+            img['_key'] = 1
+            img = left.merge(img,on='_key').drop(columns='_key')
+            imgs.append(img)
+        imgs = pd.concat(imgs)
+        self._coordinates = imgs
+        return imgs
+
+    def get_segmentation_map_images(self,type='edge',subset_logic=None,color=None,watershed_steps=0,blank=(0,0,0,255)):
+        # if subset logic is set only plot those cells
+        # if color is set color all cells that color
+        if self.verbose: sys.stderr.write("getting segmap "+str(type)+"\n")
+        ems = self.get_segmentation_maps(type=type)
+        if subset_logic is not None: 
+
+            subset = self.cdf.subset(subset_logic)
+            ems = ems.merge(subset.loc[:,subset.frame_columns+['cell_index']],on=subset.frame_columns+['cell_index'])
+        edf = ems.set_index(list(self.columns))
+        imgs = []
+        for i,r in self.iterrows():
+            imsize = r['shape']
+            img = pd.DataFrame(np.zeros(imsize))
+            if subset.shape[0] == 0: # case where there is nothing to do
+                if self.verbose: sys.stderr.write("Empty image for this phenotype subset\n")
+                imgs.append(list(r)+[img])
+                continue
+            edfsub = edf.loc[tuple(r)]
+            #if self.verbose: sys.stderr.write("make image and fill zeros\n")
+            fullx = pd.DataFrame({'x':list(range(0,imsize[1]))})
+            fullx['_key']=1
+            fully = pd.DataFrame({'y':list(range(0,imsize[0]))})
+            fully['_key']=1
+            full = fullx.merge(fully,on='_key').merge(edfsub,on=['x','y'],how='left').fillna(0)
+            img = np.array(full.pivot(columns='x',index='y',values='cell_index').astype(int))
+            #if self.verbose: sys.stderr.write("finished making image and fill zeros\n")
+            if watershed_steps > 0:
+                # get the zero and nonzero components
+                mid = map_image_ids(img,remove_zero=False)
+                midzero = list(zip(*mid.query('id==0').copy()[['x','y']].apply(tuple).tolist()))
+                mid = list(zip(*mid.query('id!=0').copy()[['x','y']].apply(tuple).tolist()))
+                img = watershed_image(np.array(img),mid,midzero,steps=watershed_steps)
+            if color is not None:
+                # we need to make a new image thats colored in
+                fresh = np.zeros(list(imsize)+[len(color)]).astype(int)
+                blank = tuple(list(blank)[0:len(color)])
+                fresh[:][:] = blank
+                # get our coordinates
+                coords = np.array(list(zip(*map_image_ids(img)[['y','x']].apply(tuple).tolist())))
+                fresh[tuple([*coords.T])] = color
+                #for i2,r2 in map_image_ids(img).iterrows():
+                #    fresh[r2['y']][r2['x']] = color
+                img = fresh
+            imgs.append(list(r)+[img])
+        imgs = pd.DataFrame(imgs,columns=list(self.columns)+['image'])
+        return imgs
+
 
     def build_segmentation_image(self,schema,background=(0,0,0,0)):
         """
@@ -89,57 +176,24 @@ class Images(Measurement):
             cummulative['new'] = cummulative.apply(lambda x: _merge_images(x['merged'],x['old']),1)
             cummulative = cummulative.drop(columns=['old','merged']).rename(columns={'new':'merged'})
         cummulative = cummulative.rename(columns={'merged':'image'})
-        return ImageOutput(cummulative)
+        return SegmentationImageOutput(cummulative)
 
     def get_outline_images(self,subset_logic=None,edge_color=(0,0,255,255),fill_color=(135,206,250,255),watershed_steps=1):
         if len(edge_color) == 3: edge_color = tuple(list(edge_color)+[255])
         if len(fill_color) == 3: fill_color = tuple(list(fill_color)+[255])
-        v = self.get_segmentation_map_images(type='edge',subset_logic=subset_logic,color=edge_color,blank=(0,0,0,0),watershed_steps=watershed_steps).\
-            rename(columns={'image':'edge'}).\
-            merge(self.get_segmentation_map_images(type='cell',subset_logic=subset_logic,color=fill_color,blank=(0,0,0,0)),on=list(self.columns)).\
+        #if self.verbose: sys.stderr.write("getting outline image\n")
+        #if self.verbose: sys.stderr.write("reading edges\n")
+        edge_images = self.get_segmentation_map_images(type='edge',subset_logic=subset_logic,color=edge_color,blank=(0,0,0,0),watershed_steps=watershed_steps).\
+            rename(columns={'image':'edge'})
+        if self.verbose: sys.stderr.write("reading cells\n")
+        cell_images = self.get_segmentation_map_images(type='cell',subset_logic=subset_logic,color=fill_color,blank=(0,0,0,0)).\
             rename(columns={'image':'cell'})
+        if self.verbose: sys.stderr.write("merge edge and cell\n")
+        v = edge_images.merge(cell_images,on=list(self.columns))
         v['merged'] = v.apply(lambda x: _merge_images(x['edge'],x['cell']),1)
+        #if self.verbose: sys.stderr.write("finished outline image\n")
         return v.drop(columns=['cell','edge'])
 
-    def get_segmentation_map_images(self,type='edge',subset_logic=None,color=None,watershed_steps=0,blank=(0,0,0,255)):
-        # if subset logic is set only plot those cells
-        # if color is set color all cells that color
-        ems = self.get_segmentation_maps(type=type)
-        if subset_logic is not None: 
-            #print(subset_logic)
-            subset = self.cdf.subset(subset_logic)
-            #print(subset.shape)
-            ems = ems.merge(subset.loc[:,subset.frame_columns+['cell_index']],on=subset.frame_columns+['cell_index'])
-        edf = ems.set_index(list(self.columns))
-        imgs = []
-        for i,r in self.iterrows():
-            edfsub = edf.loc[tuple(r)]
-            imsize = r['shape']
-            img = pd.DataFrame(np.zeros(imsize))
-            img = np.array(edfsub.pivot(columns='x',index='y',values='cell_index').\
-                loc[range(0,imsize[0]),range(0,imsize[1])].fillna(0).astype(int))
-            if watershed_steps > 0:
-                # get the zero and nonzero components
-                mid = map_image_ids(img,remove_zero=False)
-                midzero = list(zip(*mid.query('id==0').copy()[['x','y']].apply(tuple).tolist()))
-                mid = list(zip(*mid.query('id!=0').copy()[['x','y']].apply(tuple).tolist()))
-                img = watershed_image(np.array(img),mid,midzero,steps=watershed_steps)
-            if color is not None:
-                # we need to make a new image thats colored in
-                fresh = np.zeros(list(imsize)+[len(color)]).astype(int)
-                blank = tuple(list(blank)[0:len(color)])
-                fresh[:][:] = blank
-                # get our coordinates
-                coords = np.array(list(zip(*map_image_ids(img)[['y','x']].apply(tuple).tolist())))
-                fresh[tuple([*coords.T])] = color
-                #for i2,r2 in map_image_ids(img).iterrows():
-                #    fresh[r2['y']][r2['x']] = color
-                img = fresh
-                #print(type(img))
-                #imgdf = pd.DataFrame(img).applymap(lambda x: blank if x ==0 else color)
-
-            imgs.append(list(r)+[img])
-        return pd.DataFrame(imgs,columns=list(self.columns)+['image'])
 
     def get_segmentation_maps(self,type='edge'):
         if type == 'edge' and self._edge_map_cache is not None: return self._edge_map_cache
@@ -183,3 +237,4 @@ def _merge_images(image1,image2):
     pcell = Image.open(bcell)
     pcell.paste(pedge, (0, 0), pedge)
     return np.array(pcell)
+
